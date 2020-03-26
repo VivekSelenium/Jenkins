607bb111fd405c38e4e7c963b9e5a26adaf38c3f
VivekSelenium <vivek.rajendran@gmail.com>
2020-03-26 21:20:03 +0530

    code change

--- src/main/java/pages/SearchResultPage.java
+++ src/main/java/pages/SearchResultPage.java
@@ -4,7 +4,7 @@ import projectBase.ProjectSpecificMethods;
 
 public class SearchResultPage extends ProjectSpecificMethods {
 	public SearchResultPage searchResultsPage() throws InterruptedException {
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 		String currency = driver.findElementByXPath("//span[text()='Fastest']//parent::a//esky-price//span[1]").getText();
 		String price = driver.findElementByXPath("//span[text()='Fastest']//parent::a//esky-price//span[3]").getText();
 		System.out.println("The Fastest Flight price is: "+ currency + price);
