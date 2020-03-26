cc2c7c3ba3a2c9c50b9a98d0c3289e35edcb98cb
VivekSelenium <vivek.rajendran@gmail.com>
2020-03-26 20:27:28 +0530

    new commit

--- src/main/java/pages/SearchResultPage.java
+++ src/main/java/pages/SearchResultPage.java
@@ -3,7 +3,8 @@ package pages;
 import projectBase.ProjectSpecificMethods;
 
 public class SearchResultPage extends ProjectSpecificMethods {
-	public SearchResultPage searchResultsPage() {
+	public SearchResultPage searchResultsPage() throws InterruptedException {
+		Thread.sleep(5000);
 		String currency = driver.findElementByXPath("//span[text()='Fastest']//parent::a//esky-price//span[1]").getText();
 		String price = driver.findElementByXPath("//span[text()='Fastest']//parent::a//esky-price//span[3]").getText();
 		System.out.println("The Fastest Flight price is: "+ currency + price);
